4fe98cb4d42c4973afaae62986964a86546293a5
xuannghia <nghiagod121122@gmail.com>
2023-07-14 14:04:24 +0700

    initialize project

---
+++ src/main/java/com/management/adqualtechschool/AdqualtechSchoolApplication.java
@@ -0,0 +1,13 @@
+package com.management.adqualtechschool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdqualtechSchoolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdqualtechSchoolApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/management/adqualtechschool/AdqualtechSchoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.management.adqualtechschool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdqualtechSchoolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
