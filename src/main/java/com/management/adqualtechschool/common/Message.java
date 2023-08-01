116cadaa4f7c8303df9bd3e90cf98cfd509ea9bc
xuannghia <nghiagod121122@gmail.com>
2023-08-01 17:53:07 +0700

    feature/T3_events_crud add files for fix value in common as message and type

---
+++ src/main/java/com/management/adqualtechschool/common/Message.java
@@ -0,0 +1,24 @@
+package com.management.adqualtechschool.common;
+
+public class Message {
+    public final static String SUCCESS = "success";
+    public final static String FAILED = "failed";
+    public final static String DELETE_EVENT_SUCCESS = "Xóa sự kiện thành công!";
+    public final static String DELETE_EVENT_FAILED = "Xóa sự kiện không thành công!";
+    public final static String EVENT_TITLE_NOT_EMPTY = "Tên tiêu đề sự kiện không được để trống!";
+    public final static String EVENT_CONTENT_NOT_EMPTY = "Nội dung sự kiện không được để trống!";
+    public final static String EVENT_IMAGE_NOT_EMPTY = "Ảnh cho sự kiện không được để trống!";
+    public final static String EVENT_START_AT_NOT_NULL = "Thời gian sự kiện diễn ra không được để trống!";
+    public final static String EVENT_END_AT_NOT_NULL = "Thời gian sự kiện kết thúc không được để trống!";
+    public final static String CREATE_EVENT_SUCCESS = "Thêm sự kiện thành công!";
+    public final static String CREATE_EVENT_FAILED = "Thêm sự kiện không thành công!";
+    public final static String UPDATE_EVENT_SUCCESS = "Cập nhật sự kiện thành công!";
+    public final static String UPDATE_EVENT_FAILED = "Cập nhật sự kiện không thành công!";
+    public final static String START_BEFORE_END = "Thời gian diễn ra luôn trước thời gian kết thúc sự kiện!";
+    public final static String CREATE_BEFORE_START = "Thời gian diễn ra luôn trước thời gian kết thúc sự kiện!";
+    public final static String NOT_FOUND_ACCOUNT_ID = "Not found account by id!";
+    public final static String NOT_FOUND_ACCOUNT_USERNAME = "Not found account by username ";
+    public final static String NOT_FOUND_EVENT = "Not found event!";
+    public final static String NOT_FOUND_SCOPE= "Not found scope ";
+    public final static String NO_IMAGE= "Not found image name!";
+}
