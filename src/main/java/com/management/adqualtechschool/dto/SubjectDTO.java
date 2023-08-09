a7059b6bfc789433b4a4c535fac3908871f1d68e
xuannghia <nghiagod121122@gmail.com>
2023-08-09 21:21:38 +0700

    feature/T9_teachers_crud add subject object

---
+++ src/main/java/com/management/adqualtechschool/dto/SubjectDTO.java
@@ -0,0 +1,16 @@
+package com.management.adqualtechschool.dto;
+
+import com.management.adqualtechschool.entity.PrepareExam;
+import java.time.LocalDateTime;
+import java.util.List;
+import lombok.Data;
+
+@Data
+public class SubjectDTO {
+    private Long id;
+    private String name;
+    private Long unit;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+    private List<PrepareExam> prepareExams;
+}
---
+++ src/main/java/com/management/adqualtechschool/service/SubjectService.java
@@ -0,0 +1,9 @@
+package com.management.adqualtechschool.service;
+
+import com.management.adqualtechschool.dto.SubjectDTO;
+import java.util.List;
+
+public interface SubjectService {
+    List<SubjectDTO> getAllSubject();
+    SubjectDTO getSubjectByName(String name);
+}
